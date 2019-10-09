6c3f4d74fe2df849ad7dece54123fd3037ee5e90
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-09 19:03:45 -0300

    Melhorando a descrição da lógica de acerto do alvo

--- js/main.js
+++ js/main.js
@@ -57,7 +57,9 @@ let points = 0;
 /* Função de tempo para atualizar a tela */
     setInterval(updateScreen, 1500);
 
-/* Função para disparar o dardo no alvo e somar pontuação */    
+/* Função para disparar o dardo no alvo e somar pontuação,
+obtendo a posição e comparando com o ponto do centro do alvo
+para saber se acertou */    
     function shoot(event){
         
         var x = event.pageX - screen.offsetLeft;
