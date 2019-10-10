d29a20277fce695059077af465f058229a25efbc
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-10 19:34:44 -0300

    Adicionado caixas de dialog de vencedor e perdedor e logica com DOM para as mesmas no .js, selectors no .css para esconder e exibir o conteudo, conteudo de instruções do jogo.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-
 // debugger;
 
 
 
 /* Função para iniciar o jogo */
- function startGame(){
+function startGame() {
 
-/* Variavel acumuladora de pontuação e timer */    
+    /* Variavel acumuladora de pontuação, timer e velocidade */
     let points = 0;
     let timer = 20;
 
-/* Define a area de renderiação do jogo */
+    /* Define a area de renderiação do jogo */
     let screen = document.querySelector('canvas');
     let brush = screen.getContext('2d');
-    
+
     brush.fillStyle = '#12342A';
     brush.fillRect(0, 0, 1000, 400);
 
-/* Variaveis para desenhar o alvo e gerar aleatoriamente  */
+    /* Variaveis para desenhar o alvo e gerar aleatoriamente  */
     let radius = 20;
     let xRandom;
     let yRandom;
 
-/* Função para desenhar alvo */    
+    /* Função para desenhar alvo */
     function draw(x, y, radius, color) {
 
         brush.fillStyle = color;
@@ -31,43 +30,51 @@
         brush.fill();
     }
 
-/* Função para limpar a tela  */
+    /* Função para limpar a tela  */
     function clearScreen() {
         brush.clearRect(0, 0, 1000, 400);
     }
 
-/* Função para definir as areas do alvo */
-    function drawTarget(x, y){
-        draw(x, y, radius + 30, 'red');
-        draw(x, y, radius + 20, 'white');
+    /* Função para definir as areas do alvo */
+    function drawTarget(x, y) {
+        draw(x, y, radius + 30, '#365880');
+        draw(x, y, radius + 20, '#77acde');
+        draw(x, y, radius + 10, '#f0d933');
         draw(x, y, radius, 'red');
+        draw(x, y, radius - 5, '#ece5d5');
     }
 
-/* Função para sortear a área aleatória do alvo  */
+    /* Função para sortear a área aleatória do alvo  */
     function draftPosition(max) {
         return Math.floor(Math.random() * max);
     }
 
-/* Função para atualizar a tela */
-    function updateScreen(){
+    /* Função para atualizar a tela */
+    function updateScreen() {
         clearScreen();
         xRandom = draftPosition(1000);
         yRandom = draftPosition(400);
 
         drawTarget(xRandom, yRandom);
-        
+
         timer -= 1;
         let score = document.querySelector('.timer');
         score.innerText = timer;
     }
 
-/* Função de tempo para atualizar a tela */
-    
-    function stopGame(){
-        if(timer === 0){
-        clearInterval(stopInterval);
-        clearScreen();
-
+    /* Função de tempo para atualizar a tela
+     e caixas de dialogo para vencedor e perdedor */
+    function stopGame() {
+        if (timer == 0) {
+            clearInterval(stopInterval);
+            clearScreen();
+            if (points == 0) {
+                document.querySelector('.lose').classList.remove('lose-hidden');
+                document.querySelector('.lose').classList.add('lose-show');
+            } else if (points > 0) {
+                document.querySelector('.win').classList.remove('win-hidden');
+                document.querySelector('.win').classList.add('win-show');
+            }
         }
     }
 
@@ -76,35 +83,31 @@
         stopGame();
     }, 1000);
 
-//===========================================================   
+    //===========================================================   
 
-/* Função para disparar o dardo no alvo e somar pontuação,
-obtendo a posição e comparando com o ponto do centro do alvo
-para saber se acertou */    
-    function shoot(event){
-        
-        var x = event.pageX - screen.offsetLeft;
-        var y = event.pageY - screen.offsetTop;
+    /* Função para disparar o dardo no alvo e somar pontuação,
+    obtendo a posição e comparando com o ponto do centro do alvo
+    para saber se acertou */
+    function shoot(event) {
 
-        if ((x > xRandom - radius)
-            &&(x < xRandom + radius)
-            &&(y > yRandom - radius)
-            &&(y < yRandom + radius)){
+        let x = event.pageX - screen.offsetLeft;
+        let y = event.pageY - screen.offsetTop;
 
-        clearScreen();
-        
-        points += 1;
-        let score = document.querySelector('.score');
-        score.innerText = points;
-       }
+        if (x > xRandom - radius && x < xRandom + radius && y > yRandom - radius && y < yRandom + radius) {
+            clearScreen();
+
+            points += 1;
+            let score = document.querySelector('.score');
+            score.innerText = points;
+        }
     }
 
-/* Evento para capturar e chamar função de disparar o dardo */    
+    /* Evento para capturar e chamar função de disparar o dardo */
     screen.onclick = shoot;
 }
 
 /* Carregar a tela do jogo */
-window.onload = function(){
+window.onload = function () {
     let pressStart = document.querySelector('.start');
     pressStart.onclick = startGame;
 };
